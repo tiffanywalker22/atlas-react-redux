0d660f938c49ba309cf0de3facbb3b712e06f685
tiffany <tbcasper16@gmail.com>
2024-09-16 16:29:06 -0500

    mistake in getting the logo to show up; had to import it like i did in past project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from '../assets/logo.png'
 
 const Header: React.FC = () => {
     return (
         <header className="sticky top-0 border-b-2 border-blue bg-off-white-light pb-8 pt-8">
-            <img className="logo mx-auto w-56" src="../assets/logo.png" alt="logo" />
+            <img className="logo mx-auto w-56" src={Logo} alt="logo" />
         </header>
     );
 };
